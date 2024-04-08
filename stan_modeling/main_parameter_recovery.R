6763be08043485e463ada24f34a1a17a9c378367
shirelch <elchayani@mail.tau.ac.il>
2024-04-08 10:06:40 +0300

    added plots to main

--- stan_modeling/main_parameter_recovery.R
+++ stan_modeling/main_parameter_recovery.R
@@ -5,8 +5,8 @@ source('./functions/my_starter.R')
 path = set_workingmodel()
 
 cfg = list(
-  Nsubjects        = 50,
-  Nblocks          = 2,
+  Nsubjects        = 200,
+  Nblocks          = 1,
   Ntrials_perblock = 50,
   Narms            = 2, #number of arms in the task
   Nraffle          = 2, #number of arms offered for selection each trial
@@ -16,6 +16,35 @@ cfg = list(
 
 #####Simulate data--------------------
 generate_artificial_data(cfg = cfg)
+
+df = get_df(mydatatype = set_datatype(),path, standata = F) 
+last_5_trials <- df %>%
+  group_by(subject, beta) %>%
+  arrange(desc(trial)) %>%
+  filter(row_number() <= 5) %>%
+  summarise(mean_brightness_diff = mean(brightness2-brightness1))
+
+plot(last_5_trials$beta, last_5_trials$mean_brightness_diff,
+     xlab = "Beta",
+     ylab = "Mean Brightness Diffrences in Last 5 Trials",
+     main = "Beta vs. Mean Brightness Diffrences in Last 5 Trials")
+
+
+rt = hist(df$rt)
+
+accuracy = df%>%group_by(subject)%>%summarise(accuracy_rate = sum(selected_offer)/length(selected_offer))
+rt = df%>%group_by(subject)%>%summarise(mean_rt = mean(rt))
+
+beta = df%>%
+plot(accuracy)
+
+# Assuming df is your dataframe
+ggplot(df, aes(y = brightness2, x = rt)) +
+  geom_point(size = 1) +  # Increase point size
+  geom_smooth(method = "lm", se = FALSE) +  # Add linear regression line
+  labs(x = "Response Time", y = "Brightness Differences", 
+       title = "Response Time vs. Brightness Difficulty") +  # Add labels and title
+  theme_minimal() # Set a minimalist theme
 
 #####sample posterior--------------------
 
@@ -28,8 +57,8 @@ modelfit_mcmc(
   
   mymcmc = list(
     datatype = 'artificial' ,
-    samples  = 1,
-    warmup  = 2,
+    samples  = 200,
+    warmup  = 700,
     chains  = 4,
     cores   = 4
   )
@@ -48,7 +77,7 @@ examine_individual_parameters_recovery(path)
 ####examine model
 #load parameters
 fit   = readRDS(paste0(path$data, '/modelfit_recovery.rds'))
-גע
+
 Qdiff = fit$draws(variables = 'Qdiff_external', format = 'draws_matrix')
 Qval1 = fit$draws(variables = 'Qval1_external', format = 'draws_matrix')
 Qval2 = fit$draws(variables = 'Qval2_external', format = 'draws_matrix')
